313d36891d7d8607afc118f504b311bcbbab9308
Maarten ter Huurne <maarten@treewalker.org>
2015-12-19 17:07:14 +0100

    Moved GameOption class from 'enums' to 'account_info' module

--- hearthy/bnet/game_utilities_server.py
+++ hearthy/bnet/game_utilities_server.py
@@ -1,9 +1,8 @@
 import logging
 from hsproto import PegasusUtil_pb2, PegasusShared_pb2
 from hearthy.bnet import rpcdef
-from hearthy.bnet.account_info import AccountInfo
+from hearthy.bnet.account_info import AccountInfo, GameOption
 from hearthy.protocol import game_utilities, mtypes
-from hearthy.protocol.enums import GameOption
 
 ASSETS_VERSION = 7553
 
